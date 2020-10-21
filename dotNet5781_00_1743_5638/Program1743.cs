49494a9566a17239491ab74c208e1d060333cb5c
hillel94000 <hillelpinto3@gmail.com>
2020-10-22 00:17:20 +0300

    hey

---
+++ dotNet5781_00_1743_5638/Program1743.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doNet5781_00_1743_5638
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1743();
+            Welcome5638();
+            Console.ReadKey();
+        }
+        static partial void Welcome5638();
+
+        private static void Welcome1743()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
---
+++ dotNet5781_00_1743_5638/Program5638.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_00_1743_5638
+{
+    partial class Program
+    {
+        static partial void Welcome5638()
+        {
+
+            Console.WriteLine("I'm also here");
+        }
+
+
+
+    }
+}
