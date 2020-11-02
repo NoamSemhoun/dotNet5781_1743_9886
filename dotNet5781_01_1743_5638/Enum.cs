130c37442917e1d8b665d69b2158e8f4a9ba2dd9
hillel94000 <hillelpinto3@gmail.com>
2020-11-02 21:07:53 +0200

    END

--- dotNet5781_01_1743_5638/Enum.cs
+++ dotNet5781_01_1743_5638/Enum.cs
@@ -1,1 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace dotNet5781_01_1743_5638
+{
+    public enum Actions { ADD, FIND, MAINTENANCE, REFUEL, PRINT, EXIT }
+}
