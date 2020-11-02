17c99df1cc4e67e40ef71c355d810976e62167d1
NoamSemhoun <noam@semhoun.eu>
2020-11-02 18:19:38 +0100

    ajouter le TARGIL 1

---
+++ dotNet5781_01_1743_5638/Program.cs
@@ -0,0 +1,18 @@
+﻿// TARGIL 1 :   BUS
+
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace dotNet5781_01_1743_5638
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
