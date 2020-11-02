c1f87540abfb136de23af54138be7ccd8b554cc6
NoamSemhoun <noam@semhoun.eu>
2020-11-02 18:23:00 +0100

    Initial test cerate Targil 2 !! repos

---
+++ dotNet5781_02_1743_5638/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+// Test targil 2
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dotNet5781_02_1743_5638
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
