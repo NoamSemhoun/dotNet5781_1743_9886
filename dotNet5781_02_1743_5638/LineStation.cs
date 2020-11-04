ac0fe163552050c0d7e3c07f3a4d1d4fec828338
NoamSemhoun <noam@semhoun.eu>
2020-11-04 15:35:22 +0100

    stam

--- dotNet5781_02_1743_5638/LineStation.cs
+++ dotNet5781_02_1743_5638/LineStation.cs
@@ -6,13 +6,32 @@ using System.Threading.Tasks;
 
 namespace dotNet5781_02_1743_5638
 {
+    enum Area { General,North,South,Center,Jerusalem }
     class LineStation
     {
+        // meme qu'avant avec en plus, 
         int distance; // avec la station précedente
         int time;     // avec la station précedente
+
+        // object 
     }
 
     class Line  // simple  List
+    {
+        List<string> trajet;            // masloul
+
+        int Kav ; 
+        int FirstStation, LastStation ;
+
+        // 1 et 2 
+        bool SearchStation(int Kav,int code); // code de la station sur une ligne
+        int Distance(int NumStation1, int NumStation2); // distance entre les 2 station
+        int Time(int NumStation1, int NumStation2); // temp  entre les 2 station
+        // 6 ET 7 icomparable
+
+    }
+
+    class NewLine    // que 2 fois la mm ligne : dans les 2 sens 
     {
 
     }
--- dotNet5781_02_1743_5638/Station.cs
+++ dotNet5781_02_1743_5638/Station.cs
@@ -9,10 +9,11 @@ namespace dotNet5781_02_1743_5638
     class Station
     {
 
-        int code;// 6 chiifres max
+        int NumStation;// code à 6 chiifres max
 
+        //Position     	U+00B0 
         double Latitude;   // -90, 90     ISR : [31,33.3]
-        double Longitude;  // -180, 180   ISR : [34.3,35.5]
+        double Longitude;  // -180, 180   ISR : [34.3,35.5]   
         string adress;
     }
 }
