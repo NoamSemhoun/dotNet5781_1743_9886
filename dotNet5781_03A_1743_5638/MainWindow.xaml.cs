d19dece70762c34e279bcd159dff8b70fe63aba1
NoamSemhoun <noam@semhoun.eu>
2020-11-17 21:42:52 +0100

    marche pas cette merde

--- dotNet5781_03A_1743_5638/MainWindow.xaml.cs
+++ dotNet5781_03A_1743_5638/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using dotNet5781_02_1743_5638;
 
 namespace dotNet5781_03A_1743_5638
 {
@@ -20,16 +21,39 @@ namespace dotNet5781_03A_1743_5638
     /// </summary>
     /// 
     
+    
     public partial class MainWindow : Window
     {
-        // reference avec le projet du targil 2 
-        public MainWindow()
+
+        private Line currentDisplayBusLine;
+       
+
+
+        public  MainWindow()  // ctor
         {
             InitializeComponent();
-            // Ossef
+            
+
+            cbBusLines.ItemsSource = listLine;
+            cbBusLines.DisplayMemberPath = " BusLineNum ";
+            cbBusLines.SelectedIndex = 0;
+            //ShowBusLine(……….)
+
         }
 
         private void cbBusLines_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //ShowBusLine((cbBusLines.SelectedValue as Line).Bus);
+            
+        }
+        private void ShowBusLine(int index)
+        {
+            currentDisplayBusLine = listLine [index];
+            UpGrid.DataContext = currentDisplayBusLine;
+            lbBusLineStations.DataContext = currentDisplayBusLine.Stations;
+        }
+
+        private void cbBusLines_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
         {
 
         }
