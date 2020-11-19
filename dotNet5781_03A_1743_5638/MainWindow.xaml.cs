53bb459e18dfe170dc9d25dfb8b1a72d800d706f
NoamSemhoun <noam@semhoun.eu>
2020-11-19 21:12:04 +0100

    noam grph

--- dotNet5781_03A_1743_5638/MainWindow.xaml.cs
+++ dotNet5781_03A_1743_5638/MainWindow.xaml.cs
@@ -32,6 +32,9 @@ namespace dotNet5781_03A_1743_5638
             cbBusLines.ItemsSource = h.listLine;
             cbBusLines.DisplayMemberPath = "BusLineNumber";
             cbBusLines.SelectedIndex = 0;
+            ShowBusLine(cbBusLines.SelectedIndex) ;   // index ou aussi kav list 
+                                                     
+            // lbBusLineStations.datacontext = cbBusLines.item.stations;
         }
 
         private void cbBusLines_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
@@ -43,6 +46,15 @@ namespace dotNet5781_03A_1743_5638
             currentDisplayBusLine = h[index];
             UpGrid.DataContext = currentDisplayBusLine.BusLineNumber;
             lbBusLineStations.DataContext = currentDisplayBusLine.listStations;
+            tbArea_TextChanged(currentDisplayBusLine);
+        }
+
+      
+
+        private void tbArea_TextChanged(dotNet5781_02_1743_5638.Line currentDisplayBusLine)
+        {
+            // afiicher la zone Area de la ligne 
+            tbArea.DataContext = currentDisplayBusLine.area;
         }
 
         private void lbBusLineStations_SelectionChanged(object sender, SelectionChangedEventArgs e)
